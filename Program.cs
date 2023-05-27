be21afa38fb4a7cee7703ad277a859482ffc5fd6
ritesh.singh@locobuzz.com <ritesh.singh@locobuzz.com>
2023-05-27 18:43:48 +0530

    4th commit

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ class Program
             lstBrands = lstBrands.Where(b => b.BrandID == Convert.ToInt64(BrandID)).ToList();
         }
         Console.WriteLine("Brand Found :"+lstBrands.Count);
-        Console.WriteLine("Added github trigger webhooks");
+        Console.WriteLine("Added github webhooks trigger");
         foreach (BrandDetails brand in lstBrands)
         {
             Console.WriteLine("Running For Brand :" + brand.BrandName);
