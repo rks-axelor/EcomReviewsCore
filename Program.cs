b040ea7cce016685b50aa600143893e81ccef6a5
ritesh.singh@locobuzz.com <ritesh.singh@locobuzz.com>
2023-05-27 19:31:44 +0530

    beckup testing

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ class Program
             lstBrands = lstBrands.Where(b => b.BrandID == Convert.ToInt64(BrandID)).ToList();
         }
         Console.WriteLine("Brand Found :"+lstBrands.Count);
-        Console.WriteLine("Checking for Ritesh 4th time");
+        Console.WriteLine("Checking for Ritesh backup checking");
         foreach (BrandDetails brand in lstBrands)
         {
             Console.WriteLine("Running For Brand :" + brand.BrandName);
