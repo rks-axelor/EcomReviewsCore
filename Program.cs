3dedf48675846e422473b736dd315fc5f4293896
ritesh.singh@locobuzz.com <ritesh.singh@locobuzz.com>
2023-05-28 09:19:48 +0530

    onlt date correction

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ class Program
             lstBrands = lstBrands.Where(b => b.BrandID == Convert.ToInt64(BrandID)).ToList();
         }
         Console.WriteLine("Brand Found :"+lstBrands.Count);
-        Console.WriteLine("DateTime is still noyt working");
+        Console.WriteLine("Date is still not coming");
         foreach (BrandDetails brand in lstBrands)
         {
             Console.WriteLine("Running For Brand :" + brand.BrandName);
